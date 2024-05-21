31982a140a9b1eaff19b6d0286cf6e6aa4523f9d
sjeongab <sjeongab@connect.ust.hk>
2024-05-22 07:23:28 +0800

    initial commit
    initial commit

---
+++ main.js
@@ -0,0 +1,223 @@
+import {vs, fs} from "./shader.js";
+let camera = {
+    width: 100,
+    height: 100,
+    position: [5, 0, 1],
+    rotation: [
+        [-0.026919994628162257, -0.1565891128261527, -0.9872968974090509],
+        [0.08444552208239385, 0.983768234577625, -0.1583319754069128],
+        [0.9960643893290491, -0.0876350978794554, -0.013259786205163005],
+    ],
+    fy: 100,
+    fx: 100,
+};
+
+/*function parseOBJ(text) {
+    // because indices are base 1 let's just fill in the 0th data
+    const objPositions = [[0, 0, 0]];
+    const objTexcoords = [[0, 0]];
+    const objNormals = [[0, 0, 0]];
+    const objColors = [[0, 0, 0]];
+  
+    // same order as `f` indices
+    const objVertexData = [
+      objPositions,
+      objTexcoords,
+      objNormals,
+      objColors,
+    ];
+  
+    // same order as `f` indices
+    let webglVertexData = [
+      [],   // positions
+      [],   // texcoords
+      [],   // normals
+      [],   // colors
+    ];
+  
+    const materialLibs = [];
+    const geometries = [];
+    let geometry;
+    let groups = ['default'];
+    let material = 'default';
+    let object = 'default';
+  
+    const noop = () => {};
+  
+    function newGeometry() {
+      // If there is an existing geometry and it's
+      // not empty then start a new one.
+      if (geometry && geometry.data.position.length) {
+        geometry = undefined;
+      }
+    }
+  
+    function setGeometry() {
+      if (!geometry) {
+        const position = [];
+        const texcoord = [];
+        const normal = [];
+        const color = [];
+        webglVertexData = [
+          position,
+          texcoord,
+          normal,
+          color,
+        ];
+        geometry = {
+          object,
+          groups,
+          material,
+          data: {
+            position,
+            texcoord,
+            normal,
+            color,
+          },
+        };
+        geometries.push(geometry);
+      }
+    }
+  
+    function addVertex(vert) {
+      const ptn = vert.split('/');
+      console.log(ptn);
+      ptn.forEach((objIndexStr, i) => {
+        if (!objIndexStr) {
+          return;
+        }
+        const objIndex = parseInt(objIndexStr);
+        const index = objIndex + (objIndex >= 0 ? 0 : objVertexData[i].length);
+        webglVertexData[i].push(...objVertexData[i][index]);
+        // if this is the position index (index 0) and we parsed
+        // vertex colors then copy the vertex colors to the webgl vertex color data
+        if (i === 0 && objColors.length > 1) {
+          geometry.data.color.push(...objColors[index]);
+        }
+      });
+    }
+  
+    const keywords = {
+      v(parts) {
+        // if there are more than 3 values here they are vertex colors
+        if (parts.length > 3) {
+          objPositions.push(parts.slice(0, 3).map(parseFloat));
+          objColors.push(parts.slice(3).map(parseFloat));
+        } else {
+          objPositions.push(parts.map(parseFloat));
+        }
+      },
+      vn(parts) {
+        objNormals.push(parts.map(parseFloat));
+      },
+      vt(parts) {
+        // should check for missing v and extra w?
+        objTexcoords.push(parts.map(parseFloat));
+      },
+      f(parts) {
+        setGeometry();
+        const numTriangles = parts.length - 2;
+        for (let tri = 0; tri < numTriangles; ++tri) {
+          addVertex(parts[0]);
+          addVertex(parts[tri + 1]);
+          addVertex(parts[tri + 2]);
+        }
+      },
+      s: noop,    // smoothing group
+      mtllib(parts, unparsedArgs) {
+        // the spec says there can be multiple filenames here
+        // but many exist with spaces in a single filename
+        materialLibs.push(unparsedArgs);
+      },
+      usemtl(parts, unparsedArgs) {
+        material = unparsedArgs;
+        newGeometry();
+      },
+      g(parts) {
+        groups = parts;
+        newGeometry();
+      },
+      o(parts, unparsedArgs) {
+        object = unparsedArgs;
+        newGeometry();
+      },
+    };
+  
+    const keywordRE = /(\w*)(?: )*(.*)/;
+    const lines = text.split('\n');
+    for (let lineNo = 0; lineNo < lines.length; ++lineNo) {
+      const line = lines[lineNo].trim();
+      if (line === '' || line.startsWith('#')) {
+        continue;
+      }
+      const m = keywordRE.exec(line);
+      if (!m) {
+        continue;
+      }
+      const [, keyword, unparsedArgs] = m;
+      const parts = line.split(/\s+/).slice(1);
+      const handler = keywords[keyword];
+      if (!handler) {
+        console.warn('unhandled keyword:', keyword);  // eslint-disable-line no-console
+        continue;
+      }
+      handler(parts, unparsedArgs);
+    }
+  
+    // remove any arrays that have no entries.
+    for (const geometry of geometries) {
+      geometry.data = Object.fromEntries(
+          Object.entries(geometry.data).filter(([, array]) => array.length > 0));
+    }
+  
+    return {
+      geometries,
+      materialLibs,
+    };
+  }
+
+*/
+  
+
+async function main() {
+    console.log(vs);
+    document.getElementById("spinner").style.display = "none";
+    const canvas = document.getElementById("canvas");
+    //const fps = document.getElementById("fps");
+    //const camid = document.getElementById("camid");
+    const gl = canvas.getContext("webgl2", {
+        antialias: false,
+    });
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, vs);
+    gl.compileShader(vertexShader);
+    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS))
+        console.error(gl.getShaderInfoLog(vertexShader));
+
+    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, fs);
+    gl.compileShader(fragmentShader);
+    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS))
+        console.error(gl.getShaderInfoLog(fragmentShader));
+
+    const program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    gl.useProgram(program);
+    
+    const u_projection = gl.getUniformLocation(program, "projection");
+    const u_viewport = gl.getUniformLocation(program, "viewport");
+    const u_focal = gl.getUniformLocation(program, "focal");
+    const u_view = gl.getUniformLocation(program, "view");
+
+    const indexBuffer = gl.createBuffer();
+
+    //const values = await parseOBJ('./cube.obj');
+    //console.log(values);
+    //const text = await response.text();
+}
+
+main().catch((err) => {
+    console.log(err);
+});
